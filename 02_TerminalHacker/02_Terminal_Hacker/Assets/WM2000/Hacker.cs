a3a0c867d872ecf7953dfb903e10ddb4c6531848
Atsantiago <atsantiago089@gmail.com>
2019-12-29 20:12:35 -0700

    added main menu

---
+++ 02_TerminalHacker/02_Terminal_Hacker/Assets/WM2000/Hacker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+       
+        Terminal.ClearScreen();
+        var greeting = "Hello Alex";
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 to access the school library");
+        Terminal.WriteLine("Press 2 to access NASA");
+        Terminal.WriteLine("Press 3 to access MI6");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
